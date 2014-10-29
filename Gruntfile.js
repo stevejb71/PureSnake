7cbfef93c8a1bcc690baa95e7ab193b0571e08f6
stevejb71 <stevejb71@yahoo.co.uk>
2014-10-29 20:32:49 +0800

    fix up Gruntfile to build PS

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,18 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         clean: ["gen"],
-        pscMake: {
-            lib: {
-                src:
-                    [ "purs/**/*.purs"
-                    ]
-            }
-        },
         psc: {
-            options: {},
-            all : {
-                src: ["purs/**/*.purs", 
-                      "bower_components/purescript-arrays/src/**/*.purs",
-                      "bower_components/purescript-control/src/**/*.purs",
-                      "bower_components/purescript-either/src/**/*.purs",
-                      "bower_components/purescript-foldable-traversable/src/**/*.purs",
-                      "bower_components/purescript-foreign/src/**/*.purs",
-                      "bower_components/purescript-jquery/src/**/*.purs",
-                      "bower_components/purescript-monoid/src/**/*.purs",
-                      "bower_components/purescript-tuples/src/**/*.purs",
-                      ],
-                dest: "output/snake.js"
+            options : {
+            },
+            build : {
+                src: ["src/purs/Snake.purs"],
+                dest: "gen/Snake.js"
             }
         },
-        dotPsci: ["purs/**/*.purs"]
     });
 
     grunt.loadNpmTasks("grunt-purescript");
     grunt.loadNpmTasks("grunt-contrib-clean");
 
-    grunt.registerTask("default", ["psc:all"]);
+    grunt.registerTask("default", ["psc:build"]);
 };
