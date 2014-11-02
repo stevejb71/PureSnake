ae3e22da157e5189980e5e68c68792644bb5c4d8
stevejb71 <stevejb71@yahoo.co.uk>
2014-11-02 20:40:25 +0800

    draw a snake

--- src/purs/Snake.purs
+++ src/purs/Snake.purs
@@ -1,17 +1,19 @@
 module Snake where
 
-import Canvas
 import Base
 import Control.Monad.Eff
 import Graphics.Canvas
+import Data.Traversable
 
 type Position = {x :: Number, y :: Number}
 
-type Board = {x :: Number, y :: Number}
+type Board = {size :: Number, squares :: Number}
+board = {size: 500, squares: 50}
 
 data Direction = N | W | S | E
 
 data Snake = Snake Direction [Position]
+starterSnake = Snake S [{x:5,y:5},{x:6,y:5},{x:6,y:6}]
 
 changeDirection :: Direction -> Snake -> Snake
 changeDirection d' (Snake _ ps) = Snake d' ps
@@ -25,11 +27,20 @@ moveSnake b (Snake d (p:ps)) =
                 E -> {x:p.x+1, y:p.y}
     in Snake d (p':(pop ps)) 
 
-rectAt :: Position -> Context2D -> Eff (c :: Canvas) Unit
-rectAt = undefined
+toView :: Board -> Position -> Rectangle
+toView b p = let sqSize = b.size / b.squares in {h:sqSize, w:sqSize, y:p.y*sqSize, x:p.x*sqSize}
+
+fillRectAt :: Context2D -> Board -> Position -> Eff (canvas :: Canvas) Context2D
+fillRectAt ctx b p = fillRect ctx (toView b p)
+
+fillRectsAt :: Context2D -> Board -> [Position] -> Eff (canvas :: Canvas) [Context2D]
+fillRectsAt ctx b ps = for ps (fillRectAt ctx b)
+
+drawSnake :: Context2D -> Board -> Snake -> Eff (canvas :: Canvas) [Context2D]
+drawSnake ctx b (Snake _ ps) = fillRectsAt ctx b ps
 
 startSnake = do
     canvas <- getCanvasElementById "canvas"
     ctx <- getContext2D canvas
-    _ <- fillRect ctx {h:10, w:10, y:10, x:20}
+    _ <- drawSnake ctx board starterSnake
     return Unit
