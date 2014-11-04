6f85b86bf11599d447e62ac5ad5428388e03a689
stevejb71 <stevejb71@yahoo.co.uk>
2014-11-04 20:46:04 +0800

    add dotPsci task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         clean: ["gen"],
+        dotPsci: {
+            src: ["src/purs/*.purs", "bower_components/**/src/**/*.purs"]
+        },         
         psc: {
             options : {
             },
