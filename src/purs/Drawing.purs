2abcfcc4276bde6c26688709f8599879679b49a0
stevejb71 <stevejb71@yahoo.co.uk>
2014-11-09 20:01:25 +0800

    refactoring to get inputs/results from loop function more clear

--- src/purs/Drawing.purs
+++ src/purs/Drawing.purs
@@ -11,24 +11,16 @@ import Random (randomN)
 red = "rgb(200,10,10)"
 black = "rgb(0,0,0)"
 
+type ApplePosition = Position
+
 drawSnake :: forall e. Context2D -> (Position -> Rectangle) -> Snake -> Eff (canvas :: Canvas | e) Unit
 drawSnake ctx tr (Snake _ ps) = for_ (map tr ps) $ fillRectWithStyle ctx black
 
-drawApple :: Context2D -> Number -> (Position -> Rectangle) -> Eff (canvas :: Canvas, random :: Random) Unit
-drawApple ctx sz tr = do
-    pos <- chooseApplePosition sz
-    let rect = tr pos
-    _ <- fillRectWithStyle ctx red rect
-    return unit  
+drawApple :: Context2D -> ApplePosition -> (ApplePosition -> Rectangle) -> Eff (canvas :: Canvas, random :: Random) Unit
+drawApple ctx pos tr = fillRectWithStyle ctx red (tr pos)
 
 fillRectWithStyle :: forall e. Context2D -> String -> Rectangle -> Eff (canvas :: Canvas | e) Unit
 fillRectWithStyle ctx style rect = do
     _ <- setFillStyle style ctx
     _ <- fillRect ctx rect
     return unit  
-
-chooseApplePosition :: Number -> forall e. Eff (random :: Random | e) Position
-chooseApplePosition sz = do
-    x <- randomN sz
-    y <- randomN sz
-    return $ {x:x, y:y}
--- src/purs/Snake.purs
+++ src/purs/Snake.purs
@@ -4,7 +4,8 @@ import Base
 import Control.Monad.Eff (Eff())
 import Graphics.Canvas (Canvas(), Rectangle(), getCanvasElementById, getContext2D, Context2D(), clearRect)
 import Data.Array.NonEmpty
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe, isNothing, isJust)
+import Data.Maybe.Unsafe (fromJust)
 import Control.Monad.Eff.Random (Random(), random)
 import Random (randomN)
 import Drawing
@@ -49,29 +50,44 @@ keyToDirection k =
         38 -> Just N
         _ -> Nothing
 
-type ShowApple = Boolean
+nextApplePosition :: forall e. Number -> Eff (random :: Random | e) (Maybe ApplePosition)
+nextApplePosition sz = do
+    showApple <- (\x -> x < 0.05) <$> random
+    pos <- if showApple then Just <$> (choosePosition sz) else return noApple
+    return pos
+    where choosePosition sz = do
+            x <- randomN sz
+            y <- randomN sz
+            return {x:x, y:y}
 
-shouldShowApple :: forall e. Eff (random :: Random | e) ShowApple
-shouldShowApple = (\x -> x < 0.05) <$> random
+type Result = {snake :: Snake, crashed :: Boolean, appleAt :: Maybe ApplePosition}
 
-type Result = {snake :: Snake, crashed :: Boolean, showApple :: ShowApple}
+type Input = {keyCode :: KeyCode, snake :: Snake, appleAt :: Maybe ApplePosition}
 
-mkLoop :: Eff (canvas :: Canvas) (KeyCode -> ShowApple -> Snake -> Eff (canvas :: Canvas, random :: Random) Result)
-mkLoop = do
+mkLoop :: Eff (canvas :: Canvas) (Input -> Eff (canvas :: Canvas, random :: Random) Result)
+mkLoop = do 
     canvas <- getCanvasElementById "canvas"
     ctx <- getContext2D canvas
     let board = {w: 800, h: 800, squares: 40}
     return $ loop board ctx
 
-loop :: Board -> Context2D -> KeyCode -> ShowApple -> Snake -> Eff (canvas :: Canvas, random :: Random) Result
-loop board ctx keyCode showApple s@(Snake d body) = 
-    let d' = fromMaybe d (keyToDirection keyCode)
+noApple :: Maybe ApplePosition
+noApple = Nothing
+
+direction (Snake d _) = d
+
+loop :: Board -> Context2D -> Input -> Eff (canvas :: Canvas, random :: Random) Result
+loop board ctx input = 
+    let s = input.snake
+        d = direction s
+        applePosition = input.appleAt
+        d' = fromMaybe d (keyToDirection input.keyCode)
         s' = moveSnake d' s in 
-        if isSnakeOutsideBoard board s' 
-        then return $ {snake: s', crashed: true, showApple: false}
-        else do
-            _ <- clearRect ctx $ (toView board) (snakeTail s)
-            _ <- drawSnake ctx (toView board) s'
-            showApple' <- if showApple then return true else shouldShowApple
-            _ <- if showApple' && (not showApple) then drawApple ctx board.squares (toView board) else (return unit)
-            return $ {snake: s', crashed: false, showApple: showApple'}
+    if isSnakeOutsideBoard board s' 
+    then return {snake: s', crashed: true, appleAt: noApple}
+    else do
+        _ <- clearRect ctx $ (toView board) (snakeTail s)
+        _ <- drawSnake ctx (toView board) s'
+        applePosition' <- if isNothing applePosition then nextApplePosition board.squares else return applePosition
+        _ <- if isJust applePosition' && (isNothing applePosition) then drawApple ctx (fromJust applePosition') (toView board) else (return unit)
+        return $ {snake: s', crashed: false, appleAt: applePosition'}
