28b406b1caa26b431ea34cc1cd29ba0730f01e34
stevejb71 <stevejb71@yahoo.co.uk>
2014-11-10 20:07:41 +0800

    grow snake when it eats an apple

--- src/purs/Snake.purs
+++ src/purs/Snake.purs
@@ -31,15 +31,16 @@ starterSnake = Snake S ({x:5,y:5} :| [{x:6,y:5}])
 changeDirection :: Direction -> Snake -> Snake
 changeDirection d' (Snake _ ps) = Snake d' ps
 
-moveSnake :: Direction -> Snake -> Snake
-moveSnake d (Snake _ body) = 
+moveSnake :: Direction -> Boolean -> Snake -> Snake
+moveSnake d grow (Snake _ body) = 
     let p = head body
         p' = case d of 
                 N -> {x:p.x, y:p.y-1}
                 S -> {x:p.x, y:p.y+1}
                 W -> {x:p.x-1, y:p.y}
                 E -> {x:p.x+1, y:p.y}
-    in Snake d (p' :| (pop body)) 
+        body' = if grow then p' <| body else p' :| pop body
+    in Snake d body' 
 
 toView :: Board -> Position -> Rectangle
 toView b p = { h: sqHeight, w: sqWidth, x: p.x * sqWidth, y: p.y * sqHeight }
@@ -57,7 +58,7 @@ keyToDirection k =
 
 nextApplePosition :: forall e. Number -> Eff (random :: Random | e) (Maybe ApplePosition)
 nextApplePosition sz = do
-    showApple <- (\x -> x < 0.05) <$> random
+    showApple <- (\x -> x < 0.1) <$> random
     pos <- if showApple then Just <$> (choosePosition sz) else return noApple
     return pos
     where choosePosition sz = do
@@ -68,9 +69,9 @@ nextApplePosition sz = do
 snakeHasEatenApple :: Maybe ApplePosition -> Snake -> Boolean
 snakeHasEatenApple a s = fromMaybe false $ eqPosition <$> a <*> Just (snakeHead s)
 
-type Result = {snake :: Snake, crashed :: Boolean, appleAt :: Maybe ApplePosition}
+type Result = {snake :: Snake, crashed :: Boolean, appleAt :: Maybe ApplePosition, snakeHasEatenApple :: Boolean}
 
-type Input = {keyCode :: KeyCode, snake :: Snake, appleAt :: Maybe ApplePosition}
+type Input = {keyCode :: KeyCode, snake :: Snake, appleAt :: Maybe ApplePosition, growSnake :: Boolean}
 
 mkLoop :: Eff (canvas :: Canvas) (Input -> Eff (canvas :: Canvas, random :: Random) Result)
 mkLoop = do 
@@ -88,14 +89,15 @@ loop board ctx input =
         d = direction s
         applePosition = input.appleAt
         d' = fromMaybe d (keyToDirection input.keyCode)
-        s' = moveSnake d' s in 
+        s' = moveSnake d' input.growSnake s in 
     if isSnakeOutsideBoard board s' 
-    then return {snake: s', crashed: true, appleAt: noApple}
+    then return {snake: s', crashed: true, appleAt: noApple, snakeHasEatenApple: false}
     else do
+        let hasEaten = snakeHasEatenApple applePosition s'
         _ <- clearRect ctx $ (toView board) (snakeTail s)
         _ <- drawSnake ctx (toView board) s'
         applePosition' <- if isNothing applePosition 
                           then nextApplePosition board.squares 
-                          else return $ if snakeHasEatenApple applePosition s' then noApple else applePosition
+                          else return $ if hasEaten then noApple else applePosition
         _ <- if isJust applePosition' && (isNothing applePosition) then drawApple ctx (fromJust applePosition') (toView board) else (return unit)
-        return $ {snake: s', crashed: false, appleAt: applePosition'}
+        return $ {snake: s', crashed: false, appleAt: applePosition', snakeHasEatenApple: hasEaten}
